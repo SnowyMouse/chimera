5299fdc2b48f60c29ac5e15a576912202b9dd0fd
Kavawuvi <flycoba1tdragon@gmail.com>
2019-08-28 14:41:32 -0500

    Use integer math for deadzones

--- src/chimera/command/client/controller/deadzones.cpp
+++ src/chimera/command/client/controller/deadzones.cpp
@@ -4,15 +4,17 @@
 #include "../../../chimera.hpp"
 #include "../../../output/output.hpp"
 
+static constexpr std::int16_t MAX_INPUT = 0x1000;
+static std::int16_t min_input = 0x0;
+static std::int16_t input_range = 0x0;
+
 extern "C" {
-    float deadzone_value = 0.0F;
     extern void handle_deadzones_asm();
 }
 
 extern "C" std::uint32_t handle_deadzones_cpp(std::uint32_t x) {
     std::int16_t value = static_cast<std::int16_t>(x);
-    std::int16_t absolute_value;
-    int sign;
+    std::int16_t absolute_value, sign;
     if(value < 0) {
         absolute_value = -value;
         sign = -1;
@@ -22,23 +24,16 @@ extern "C" std::uint32_t handle_deadzones_cpp(std::uint32_t x) {
         sign = 1;
     }
 
-    constexpr std::int16_t MAX_INPUT = 0x1000;
-    const std::int16_t MIN_INPUT = static_cast<std::int16_t>(MAX_INPUT * deadzone_value);
-    absolute_value -= MIN_INPUT;
-
     // If we fall before the deadzone, return 0
+    absolute_value -= min_input;
     if(absolute_value < 0) {
         return 0;
     }
 
-    // Otherwise, scale it
-    else {
-        const float INPUT_SCALE = static_cast<float>(MAX_INPUT) / (MAX_INPUT - MIN_INPUT);
-        absolute_value *= INPUT_SCALE;
-    }
-
-    if(absolute_value >= 0x1000) {
-        absolute_value = 0x1000;
+    // Multiply by the max input, divide by input range
+    absolute_value = static_cast<std::int16_t>(static_cast<std::int32_t>(MAX_INPUT) * absolute_value / input_range);
+    if(absolute_value >= MAX_INPUT) {
+        absolute_value = MAX_INPUT;
     }
 
     return absolute_value * sign;
@@ -47,19 +42,35 @@ extern "C" std::uint32_t handle_deadzones_cpp(std::uint32_t x) {
 namespace Chimera {
     bool deadzones_command(int argc, const char **argv) {
         static Hook hook;
+        static float deadzone_value = 0.0F;
+        static bool enabled = false;
         auto analog_input_addr = get_chimera().get_signature("analog_input_sig").data();
 
         if(argc == 1) {
             deadzone_value = std::stof(*argv);
             if(deadzone_value <= 0.0F) {
                 deadzone_value = 0.0F;
-                hook.rollback();
+                if(enabled) {
+                    hook.rollback();
+                    enabled = false;
+                }
             }
             else {
-                if(deadzone_value >= 0.99F) {
-                    deadzone_value = 0.99F;
+                if(deadzone_value >= 1.0F) {
+                    deadzone_value = 1.0F;
                 }
-                write_jmp_call(analog_input_addr, hook, reinterpret_cast<const void *>(handle_deadzones_asm), nullptr);
+
+                min_input = MAX_INPUT * deadzone_value;
+                if(min_input >= MAX_INPUT) {
+                    min_input = MAX_INPUT - 1;
+                }
+
+                input_range = MAX_INPUT - min_input;
+
+                if(!enabled) {
+                    write_jmp_call(analog_input_addr, hook, reinterpret_cast<const void *>(handle_deadzones_asm), nullptr);
+                    enabled = true;
+                }
             }
         }
 
