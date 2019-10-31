0aadf0cf822431aafc1709bb2c267b491a3d689d
Kavawuvi <flycoba1tdragon@gmail.com>
2019-10-31 00:50:53 -0500

    Fix camera

--- src/chimera/halo_data/camera.cpp
+++ src/chimera/halo_data/camera.cpp
@@ -1,6 +1,7 @@
 #include "camera.hpp"
 #include "../chimera.hpp"
 #include "../signature/signature.hpp"
+#include <optional>
 
 namespace Chimera {
     CameraType camera_type() noexcept {
@@ -9,7 +10,10 @@ namespace Chimera {
     }
 
     CameraData &camera_data() noexcept { //0x647600 usually
-        static auto *camera_coord_addr = *reinterpret_cast<CameraData **>(get_chimera().get_signature("camera_coord_sig").data() + 2);
-        return *camera_coord_addr;
+        static std::optional<CameraData *> camera_coord_addr;
+        if(!camera_coord_addr.has_value()) {
+            camera_coord_addr = reinterpret_cast<CameraData *>(*reinterpret_cast<std::byte **>(get_chimera().get_signature("camera_coord_sig").data() + 2) - 0x8);
+        }
+        return **camera_coord_addr;
     }
 }
