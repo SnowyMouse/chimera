27fcba2a3301e7367ab3db33978d424db796fc82
Kavawuvi <flycoba1tdragon@gmail.com>
2019-11-02 23:25:06 -0600

    Make the crash fix required

--- src/chimera/fix/force_crash.cpp
+++ src/chimera/fix/force_crash.cpp
@@ -5,7 +5,7 @@
 
 namespace Chimera {
     void set_up_force_crash_fix() noexcept {
-        if(get_chimera().feature_present("client_demo_crash")) {
+        if(get_chimera().feature_present("client_demo")) {
             const short mod[] = {0x90,0x90,0x90,0x90,0x90,0x90};
             auto *ptr = get_chimera().get_signature("force_crash_demo_sig").data();
             write_code_s(ptr, mod);
