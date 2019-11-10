2c557a7e7bf8fa64a6e0853f6a9cffbb80475966
Kavawuvi <flycoba1tdragon@gmail.com>
2019-11-10 14:48:07 -0600

    Update signatures for size from invader

--- src/chimera/map_loading/map_loading.cpp
+++ src/chimera/map_loading/map_loading.cpp
@@ -12,8 +12,8 @@
 #include <chrono>
 
 namespace Invader::Compression {
-    void decompress_map_file(const char *input, const char *output);
-    void decompress_map_file(const char *input, std::byte *output, std::size_t output_size);
+    std::size_t decompress_map_file(const char *input, const char *output);
+    std::size_t decompress_map_file(const char *input, std::byte *output, std::size_t output_size);
 }
 
 namespace Chimera {
