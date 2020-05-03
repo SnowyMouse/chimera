c7096907209837cf8d812d4b2aa1f33530389cae
Kavawuvi <flycoba1tdragon@gmail.com>
2020-05-03 00:40:37 -0500

    Motion sensor fix files

---
+++ src/chimera/fix/motion_sensor_fix.cpp
@@ -0,0 +1,23 @@
+#include "../chimera.hpp"
+#include "../signature/hook.hpp"
+#include "../signature/signature.hpp"
+#include "../event/tick.hpp"
+
+extern "C" {
+    const void *original_motion_sensor_update_function;
+    std::uint32_t can_update_motion_sensor = 0;
+    void new_motion_sensor_update_function();
+}
+
+namespace Chimera {
+    static void allow_updates() {
+        can_update_motion_sensor = 1;
+    }
+
+    void set_up_motion_sensor_fix() noexcept {
+        auto *data = get_chimera().get_signature("motion_sensor_update_sig").data();
+        static Hook hook;
+        write_function_override(data, hook, reinterpret_cast<const void *>(new_motion_sensor_update_function), &original_motion_sensor_update_function);
+        add_pretick_event(allow_updates);
+    }
+}
---
+++ src/chimera/fix/motion_sensor_fix.hpp
@@ -0,0 +1,11 @@
+#ifndef CHIMERA_MOTION_SENSOR_FIX_HPP
+#define CHIMERA_MOTION_SENSOR_FIX_HPP
+
+namespace Chimera {
+    /**
+     * Fix motion sensor being fucked at high frame rates
+     */
+    void set_up_motion_sensor_fix() noexcept;
+}
+
+#endif
