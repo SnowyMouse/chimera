3dd93be213c58813f0ae3f2936daf15a37d0b8ca
Kavawuvi <flycoba1tdragon@gmail.com>
2020-05-13 14:40:27 -0500

    Fix name setting

--- src/chimera/command/client/enhancement/set_name.cpp
+++ src/chimera/command/client/enhancement/set_name.cpp
@@ -5,22 +5,31 @@
 #include "../../../output/output.hpp"
 
 namespace Chimera {
+    static wchar_t name[13] = {};
+
     bool set_name_command(int argc, const char **argv) {
-        static wchar_t *name = nullptr;
-        if(!name) {
-            name = *reinterpret_cast<wchar_t **>(get_chimera().get_signature("player_name_sig").data() + 1) + 1;
-        }
+        auto &player_name_sig = get_chimera().get_signature("player_name_sig");
+
         if(argc) {
             std::size_t name_length = std::strlen(*argv);
-            if(name_length > 11) {
+
+            if(name_length == 0) {
+                player_name_sig.rollback(); // roll it back
+            }
+            else if(name_length > 11) {
                 console_error("Invalid name %s. Name exceeds the maximum name size of 11 characters.", *argv);
                 return false;
             }
+            else {
+                overwrite(player_name_sig.data() + 1, static_cast<wchar_t *>(name));
+            }
 
-            // Copy the name and the null terminator
-            std::copy(*argv, *argv + name_length + 1, name);
+            // Copy the name (with null terminator)
+            std::copy(*argv, *argv + name_length + 1, name + 1);
         }
-        console_output("%S", name);
+
+        console_output("%S", name + 1);
+
         return true;
     }
 }
