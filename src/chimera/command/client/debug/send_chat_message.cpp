63513c76315f688bbf182691c023084736472b84
Kavawuvi <flycoba1tdragon@gmail.com>
2020-05-20 21:16:35 -0500

    Bump the time up to be safe

--- src/chimera/command/client/debug/send_chat_message.cpp
+++ src/chimera/command/client/debug/send_chat_message.cpp
@@ -33,12 +33,12 @@ namespace Chimera {
         // Keep the player from muting themselves by mistake
         if(server_type() == ServerType::SERVER_DEDICATED) {
             auto tick_rate = effective_tick_rate();
-            if(throttle_time > tick_rate * 2.0F) {
+            if(throttle_time > tick_rate * 3.0F) {
                 console_error(localize("chimera_send_chat_message_throttled"));
                 return false;
             }
 
-            throttle_time += tick_rate * 1.5F;
+            throttle_time += tick_rate * 3.0F;
             add_tick_event(reduce_throttle);
         }
 
