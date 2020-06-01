a30fb86fead303ee5eeafdefa94e273900b73044
Kavawuvi <flycoba1tdragon@gmail.com>
2020-06-01 18:59:44 -0500

    Get more stuff working

--- src/chimera/map_loading/map_loading.cpp
+++ src/chimera/map_loading/map_loading.cpp
@@ -16,6 +16,7 @@
 #include "../halo_data/tag.hpp"
 #include "../config/ini.hpp"
 #include "../event/frame.hpp"
+#include "../event/map_load.hpp"
 #include "../../hac_map_downloader/hac_map_downloader.hpp"
 #include "../bookmark/bookmark.hpp"
 #include "../halo_data/multiplayer.hpp"
@@ -914,6 +915,99 @@ namespace Chimera {
         return 1;
     }
 
+    static void load_custom_edition_tags_into_retail_finally() {
+        for(std::size_t i = 0; i < get_tag_data_header().tag_count; i++) {
+            Tag *tag = get_tag(i);
+
+            if(tag->indexed) {
+                std::byte *base = nullptr;
+                if(tag->primary_class == TagClassInt::TAG_CLASS_SOUND) {
+                    for(std::size_t i = 0; i < sounds_custom_index.size(); i++) {
+                        if(sounds_custom_index[i] == tag->path) {
+                            base = sounds_custom[i].get() + 0xA4;
+                            *reinterpret_cast<std::byte **>(tag->data + 0x98 + 0x4) = base;
+                            break;
+                        }
+                    }
+                }
+                else {
+                    std::uint32_t index = reinterpret_cast<std::uint32_t>(tag->data);
+                    auto &thing_to_use = tag->primary_class == TagClassInt::TAG_CLASS_BITMAP ? bitmaps_custom : loc_custom;
+                    if(thing_to_use.size() > index) {
+                        base = thing_to_use[index].get();
+                        tag->data = base;
+                    }
+                }
+
+                // If none was found, give up
+                if(!base) {
+                    continue;
+                }
+
+                // Offset to work with
+                auto base_offset = reinterpret_cast<std::uintptr_t>(base);
+
+                #define INCREMENT_IF_NECESSARY(what) { \
+                    auto &ptr = *reinterpret_cast<std::byte **>(what); \
+                    if(ptr < base && ptr != 0) { \
+                        ptr += base_offset; \
+                    } \
+                }
+
+                // Now fix the pointers
+                switch(tag->primary_class) {
+                    case TagClassInt::TAG_CLASS_BITMAP: {
+                        INCREMENT_IF_NECESSARY(base + 0x54 + 0x4);
+                        INCREMENT_IF_NECESSARY(base + 0x60 + 0x4);
+                        auto sequence_count = *reinterpret_cast<std::uint32_t *>(base + 0x54);
+                        auto *sequences = *reinterpret_cast<std::byte **>(base + 0x54 + 0x4);
+                        for(std::uint32_t s = 0; s < sequence_count; s++) {
+                            INCREMENT_IF_NECESSARY(sequences + s * 64 + 0x34 + 0x4);
+                        }
+                        break;
+                    }
+                    case TagClassInt::TAG_CLASS_SOUND: {
+                        auto pitch_range_count = *reinterpret_cast<std::uint32_t *>(tag->data + 0x98);
+                        for(std::uint32_t p = 0; p < pitch_range_count; p++) {
+                            INCREMENT_IF_NECESSARY(base + p * 72 + 0x3C + 0x4);
+                        }
+                        break;
+                    }
+                    case TagClassInt::TAG_CLASS_FONT: {
+                        INCREMENT_IF_NECESSARY(base + 0x7C + 0x4);
+                        INCREMENT_IF_NECESSARY(base + 0x30 + 0x4);
+                        INCREMENT_IF_NECESSARY(base + 0x88 + 0xC);
+                        std::uint32_t table_count = *reinterpret_cast<std::uint32_t *>(base + 0x30);
+                        auto *tables = *reinterpret_cast<std::byte **>(base + 0x30 + 0x4);
+                        for(std::uint32_t t = 0; t < table_count; t++) {
+                            INCREMENT_IF_NECESSARY(tables + t * 12 + 0x0 + 0x4);
+                        }
+                        break;
+                    }
+                    case TagClassInt::TAG_CLASS_UNICODE_STRING_LIST: {
+                        INCREMENT_IF_NECESSARY(base + 0x0 + 0x4);
+                        std::uint32_t string_count = *reinterpret_cast<std::uint32_t *>(base + 0x0);
+                        std::byte *strings = *reinterpret_cast<std::byte **>(base + 0x0 + 0x4);
+                        for(std::uint32_t s = 0; s < string_count; s++) {
+                            INCREMENT_IF_NECESSARY(strings + s * 20 + 0x0 + 0xC);
+                        }
+                        break;
+                    }
+                    case TagClassInt::TAG_CLASS_HUD_MESSAGE_TEXT: {
+                        INCREMENT_IF_NECESSARY(base + 0x0 + 0xC);
+                        INCREMENT_IF_NECESSARY(base + 0x14 + 0x4);
+                        INCREMENT_IF_NECESSARY(base + 0x20 + 0x4);
+                        break;
+                    }
+                    default:
+                        break;
+                }
+
+                #undef INCREMENT_IF_NECESSARY
+            }
+        }
+    }
+
     void set_up_map_loading() {
         // Get settings
         auto is_enabled = [](const char *what) -> bool {
@@ -990,10 +1084,69 @@ namespace Chimera {
             sounds_custom_rsc = std::fopen(path_for_map(sounds_custom_map), "rb");
             loc_custom_rsc = std::fopen(path_for_map(loc_custom_map), "rb");
             if((custom_maps_on_retail = bitmaps_custom_rsc && sounds_custom_rsc && loc_custom_rsc)) {
-                overwrite(get_chimera().get_signature("retail_check_version_1_sig").data() + 7, static_cast<std::uint16_t>(0x9090));
-                overwrite(get_chimera().get_signature("retail_check_version_2_sig").data() + 4, static_cast<std::uint8_t>(0xEB));
+                auto load_external_resources = [](std::FILE *file, std::vector<std::unique_ptr<std::byte []>> &into, std::vector<std::string> *index = nullptr) {
+                    if(!custom_maps_on_retail) {
+                        return;
+                    }
+
+                    #define ASSERT_OR_BAIL_LOAD_EXT_RES(what) if(!(what)) { into.clear(); if(index) { index->clear(); } custom_maps_on_retail = false; return; };
+
+                    // Read the header
+                    std::byte header[0x10];
+                    ASSERT_OR_BAIL_LOAD_EXT_RES(std::fread(header, sizeof(header), 1, file));
+
+                    auto names = *reinterpret_cast<std::uint32_t *>(header + 0x4);
+                    auto indices = *reinterpret_cast<std::uint32_t *>(header + 0x8);
+                    auto count = *reinterpret_cast<std::uint32_t *>(header + 0xC);
+
+                    std::size_t name_size = indices - names;
+                    std::unique_ptr<char []> names_data = std::make_unique<char []>(name_size);
+                    std::fseek(file, names, SEEK_SET);
+                    ASSERT_OR_BAIL_LOAD_EXT_RES(std::fread(names_data.get(), name_size, 1, file));
+
+                    // Size things
+                    if(index) {
+                        index->resize(count);
+                    }
+                    into.resize(count);
+
+                    // Read names
+                    std::uint32_t start = file != loc_custom_rsc ? 1 : 0;
+                    std::uint32_t increment = file != loc_custom_rsc ? 2 : 1;
+
+                    for(std::uint32_t i = start; i < count; i+=increment) {
+                        std::byte item[0xC];
+                        std::fseek(file, indices + sizeof(item) * i, SEEK_SET);
+                        ASSERT_OR_BAIL_LOAD_EXT_RES(std::fread(item, sizeof(item), 1, file));
+
+                        auto name_offset = *reinterpret_cast<std::uint32_t *>(item + 0x0);
+                        auto size = *reinterpret_cast<std::uint32_t *>(item + 0x4);
+                        auto data_offset = *reinterpret_cast<std::uint32_t *>(item + 0x8);
+
+                        if(index) {
+                            (*index)[i] = names_data.get() + name_offset;
+                        }
+
+                        into[i] = std::make_unique<std::byte []>(size);
+                        std::fseek(file, data_offset, SEEK_SET);
+                        ASSERT_OR_BAIL_LOAD_EXT_RES(std::fread(into[i].get(), size, 1, file));
+                    }
+
+                    #undef ASSERT_OR_BAIL_LOAD_EXT_RES
+                };
+
+                load_external_resources(bitmaps_custom_rsc, bitmaps_custom);
+                load_external_resources(sounds_custom_rsc, sounds_custom, &sounds_custom_index);
+                load_external_resources(loc_custom_rsc, loc_custom);
+
+                if(custom_maps_on_retail) {
+                    overwrite(get_chimera().get_signature("retail_check_version_1_sig").data() + 7, static_cast<std::uint16_t>(0x9090));
+                    overwrite(get_chimera().get_signature("retail_check_version_2_sig").data() + 4, static_cast<std::uint8_t>(0xEB));
+                    add_map_load_event(load_custom_edition_tags_into_retail_finally);
+                }
             }
-            else {
+
+            if(!custom_maps_on_retail) {
                 if(bitmaps_custom_rsc) std::fclose(bitmaps_custom_rsc);
                 if(sounds_custom_rsc) std::fclose(sounds_custom_rsc);
                 if(loc_custom_rsc) std::fclose(loc_custom_rsc);
