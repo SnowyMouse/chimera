41ddda35344f68e79f780f36c62b8cd35cb5b6fc
Kavawuvi <flycoba1tdragon@gmail.com>
2020-06-02 17:17:19 -0500

    Don't break globals

--- src/chimera/fix/invalid_command_crash.cpp
+++ src/chimera/fix/invalid_command_crash.cpp
@@ -4,14 +4,14 @@
 #include "../event/tick.hpp"
 
 extern "C" {
-    void *halo_precompile_script_fn = nullptr;
+    void *halo_get_function_index_fn = nullptr;
     void handle_invalid_command_crash_asm() noexcept;
 }
 
 namespace Chimera {
     void set_up_invalid_command_crash_fix() noexcept {
-        halo_precompile_script_fn = reinterpret_cast<void *>(get_chimera().get_signature("precompile_script_sig").data());
+        halo_get_function_index_fn = reinterpret_cast<void *>(get_chimera().get_signature("get_function_index_sig").data());
         static Hook hook;
-        write_jmp_call(get_chimera().get_signature("invalid_command_script_sig").data() + 9, hook, nullptr, reinterpret_cast<const void *>(handle_invalid_command_crash_asm), false);
+        write_jmp_call(get_chimera().get_signature("get_function_indices_map_load_sig").data(), hook, nullptr, reinterpret_cast<const void *>(handle_invalid_command_crash_asm), false);
     }
 }
