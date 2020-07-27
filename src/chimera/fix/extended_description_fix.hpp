a2d4c196f0eaed06f3a22719d781e8122cde54ed
JerryBrick <ccs81.mr@gmail.com>
2020-07-27 01:20:14 -0500

    Fix extended description

---
+++ src/chimera/fix/extended_description_fix.hpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#ifndef CHIMERA_EXTENDED_DESCRIPTION_FIX_HPP
+#define CHIMERA_EXTENDED_DESCRIPTION_FIX_HPP
+
+namespace Chimera { 
+    /**
+     * Fix the extended description index of the profile settings menu
+     */
+    void set_up_extended_description_fix() noexcept;
+}
+
+#endif
