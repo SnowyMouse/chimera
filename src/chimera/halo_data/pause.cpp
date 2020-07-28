70a91b2d9b2c8efbd476f7f49ec1d4b396cd3f79
Kavawuvi <flycoba1tdragon@gmail.com>
2020-07-28 18:42:29 -0500

    Add game pause signature

--- src/chimera/halo_data/pause.cpp
+++ src/chimera/halo_data/pause.cpp
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-#include "game_engine.hpp"
-#include "pause.hpp"
+#include <optional>
+#include "../signature/signature.hpp"
+#include "../chimera.hpp"
 
 namespace Chimera {
     bool game_paused() noexcept {
-        switch(game_engine()) {
-            case GameEngine::GAME_ENGINE_DEMO:
-                return *reinterpret_cast<bool *>(0x4BAD02EA);
-            default:
-                return *reinterpret_cast<bool *>(0x400002EA);
+        static std::optional<std::byte **> paused_addr;
+        if(!paused_addr.has_value()) {
+            paused_addr = *reinterpret_cast<std::byte ***>(get_chimera().get_signature("game_paused_sig").data() + 2);
         }
+        return *reinterpret_cast<bool *>(*paused_addr.value() + 2);
     }
 }
