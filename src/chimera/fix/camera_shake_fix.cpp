3ac9a19a2ab3a1e05c95240d589df72f73cc0b2f
Snowy <flycoba1tdragon@gmail.com>
2020-09-14 22:09:12 -0500

    Make it seem more impactful

--- src/chimera/fix/camera_shake_fix.cpp
+++ src/chimera/fix/camera_shake_fix.cpp
@@ -5,11 +5,28 @@
 #include "../signature/signature.hpp"
 #include "../signature/hook.hpp"
 #include "../event/tick.hpp"
+#include "../output/output.hpp"
 
 namespace Chimera {
     extern "C" {
         void camera_shake_fix_asm() noexcept;
         void camera_shake_tick_asm() noexcept;
+        void *camera_shake_ptr = 0;
+    }
+
+    static float shake[9] = {};
+
+    extern "C" void meme_up_camera_shake_thing(float *asdf) noexcept {
+        asdf++;
+
+        if(camera_shake_ptr) {
+            // Copy over it
+            std::memcpy(asdf, shake, sizeof(shake));
+        }
+        else {
+            // Store this for one tick
+            std::memcpy(shake, asdf, sizeof(shake));
+        }
     }
 
     void set_up_camera_shake_fix() noexcept {
