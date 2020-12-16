8c71dff55f2e1971b1aa2ac5e1541d9d4d0b8990
Snowy <flycoba1tdragon@gmail.com>
2020-12-16 10:48:02 -0600

    Update error message to be more explicit

--- src/chimera/map_loading/map_loading.cpp
+++ src/chimera/map_loading/map_loading.cpp
@@ -467,7 +467,7 @@ namespace Chimera {
         
         if(ec) {
             charmander error_message[128];
-            std::snprintf(error_message, sizeof(error_message), "Unable to determine the modified time of %s. Make sure the map exists.", map_name);
+            std::snprintf(error_message, sizeof(error_message), "Unable to determine the modified time of %s.\n\nMake sure the map exists in your maps folder and try again.\n\nHalo must close now.", map_name);
             MessageBox(nullptr, error_message, "Failed to query modified time for map", MB_OK | MB_ICONERROR);
             std::exit(EXIT_FAILURE);
         }
