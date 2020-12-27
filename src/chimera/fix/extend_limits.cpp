1ef91c185cf659db15e3fb6105e05bb61e8de880
Snowy <flycoba1tdragon@gmail.com>
2020-12-27 13:51:39 -0600

    Remove old code

--- src/chimera/fix/extend_limits.cpp
+++ src/chimera/fix/extend_limits.cpp
@@ -21,13 +21,6 @@ extern "C" {
     void extend_limits_cmp_1() noexcept;
     void extend_limits_cmp_2() noexcept;
     void extend_limits_cmp_3() noexcept;
-
-    const void *bsp_render_1 = nullptr;
-    const void *bsp_render_2 = nullptr;
-    const void *bsp_render_3 = nullptr;
-    const std::byte *bsp_render_finally = nullptr;
-
-    void bsp_render_calls() noexcept;
 }
 
 namespace Chimera {
@@ -57,7 +50,6 @@ namespace Chimera {
         auto &bsp_poly_limit_3_sig = get_chimera().get_signature("bsp_poly_limit_3_sig");
         auto &bsp_poly_movsx_sig = get_chimera().get_signature("bsp_poly_movsx_sig");
         auto &bsp_poly_movsx_2_sig = get_chimera().get_signature("bsp_poly_movsx_2_sig");
-        auto &bsp_poly_bsp_render_calls_sig = get_chimera().get_signature("bsp_poly_bsp_render_calls_sig");
 
         auto &visible_object_list_1_sig = get_chimera().get_signature("visible_object_list_1_sig");
         auto &visible_object_list_2_sig = get_chimera().get_signature("visible_object_list_2_sig");
@@ -113,21 +105,6 @@ namespace Chimera {
         SigByte new_movsx[] =  { -1, -1, -1, 0x85, 0xFF, 0x90, -1, -1, -1, -1, -1, -1, -1, 0x8B, 0xD7, 0x90 };
         write_code_s(bsp_poly_movsx_2_sig.data(), new_movsx);
 
-        bsp_render_1 = reinterpret_cast<const void *>(bsp_poly_bsp_render_calls_sig.data() + 5 + *reinterpret_cast<std::uintptr_t *>(bsp_poly_bsp_render_calls_sig.data() + 1));
-        bsp_render_2 = reinterpret_cast<const void *>(bsp_poly_bsp_render_calls_sig.data() + 10 + *reinterpret_cast<std::uintptr_t *>(bsp_poly_bsp_render_calls_sig.data() + 6));
-        bsp_render_3 = reinterpret_cast<const void *>(bsp_poly_bsp_render_calls_sig.data() + 15 + *reinterpret_cast<std::uintptr_t *>(bsp_poly_bsp_render_calls_sig.data() + 11));
-        bsp_render_finally = bsp_poly_bsp_render_calls_sig.data() + 10;
-        //SigByte bsp_render_nop[] = {0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90};
-        //write_code_s(bsp_poly_bsp_render_calls_sig.data(), bsp_render_nop);
-        static Hook bsp_render_hook;
-        //const void *old;
-        //write_function_override(bsp_poly_bsp_render_calls_sig.data(), bsp_render_hook, reinterpret_cast<const void *>(bsp_render_calls), &old);
-
-        //overwrite(bsp_poly_another_limit_sig.data() + 1, extend_limits_poly_limit);
-        //overwrite(bsp_poly_limit_1_sig.data() + 0x7, static_cast<std::uint16_t>(BSP_POLY_LIMIT));
-        //overwrite(bsp_poly_limit_2_sig.data() + 0x7, static_cast<std::uint16_t>(BSP_POLY_LIMIT));
-        //overwrite(bsp_poly_limit_3_sig.data() + 0x7, static_cast<std::uint16_t>(BSP_POLY_LIMIT));
-
         // Bump up the visible object limit
         overwrite(visible_object_list_1_sig.data() + 3, visible_objects.get());
         overwrite(visible_object_list_2_sig.data() + 1, static_cast<std::uint32_t>(VISIBLE_OBJECT_LIMIT));
@@ -138,38 +115,5 @@ namespace Chimera {
         overwrite(visible_object_limit_2_sig.data() + 1, static_cast<std::uint32_t>(VISIBLE_OBJECT_LIMIT));
 
         overwrite(draw_distance_sig.data() + 1, &MAXIMUM_DRAW_DISTANCE);
-    }
-
-    // Basically we're looping through all of the BSP polygons
-    static bool in_loop = false;
-    static std::uint32_t actual_bsp_polygons = 0;
-    static std::uint32_t rendered_count = 0;
-
-    extern "C" void bsp_poly_begin_loop() {
-        if(!in_loop) {
-            actual_bsp_polygons = *extend_limits_polies_count;
-            rendered_count = 0;
-            in_loop = true;
-        }
-
-        *extend_limits_polies_count = actual_bsp_polygons - rendered_count;
-
-        if(*extend_limits_polies_count > INT16_MAX) {
-            *extend_limits_polies_count = INT16_MAX;
-        }
-
-        if(rendered_count) {
-            std::copy(bsp_polies.get() + rendered_count * 4, bsp_polies.get() + (rendered_count + *extend_limits_polies_count) * 4, bsp_polies.get());
-        }
-
-        rendered_count += *extend_limits_polies_count;
-    }
-
-    extern "C" bool bsp_poly_continue() {
-        if(rendered_count == actual_bsp_polygons) {
-            in_loop = false;
-        }
-        *extend_limits_polies_count = actual_bsp_polygons;
-        return in_loop;
     }
 }
