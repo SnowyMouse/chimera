41f0b393f78f4a2a784f6a91cdf76342234a2fa6
Snowy <snowy@totodile.io>
2021-02-05 15:03:03 -0600

    Change file size check for demo to match file descriptor max size

--- src/chimera/fix/leak_descriptors.cpp
+++ src/chimera/fix/leak_descriptors.cpp
@@ -7,18 +7,26 @@
 
 namespace Chimera {
     void set_up_fix_leaking_descriptors() noexcept {
-        if(!get_chimera().feature_present("core_file_descriptors")) {
-            return;
+        static std::uint32_t fix = 0x7F000000;
+        auto &chimera = get_chimera();
+        
+        if(chimera.feature_present("core_file_descriptors")) {
+            auto *addr1 = chimera.get_signature("leak_file_descriptors_1_sig").data();
+            overwrite(addr1 + 1, fix);
+
+            auto *addr2 = chimera.get_signature("leak_file_descriptors_2_sig").data();
+            overwrite(addr2 + 2, fix);
+
+            auto *addr3 = chimera.get_signature("leak_file_descriptors_3_sig").data();
+            overwrite(addr3 + 1, fix);
         }
-
-        static std::uint32_t fix = 0x7FFFFFFC;
-        auto *addr1 = get_chimera().get_signature("leak_file_descriptors_1_sig").data();
-        overwrite(addr1 + 1, fix);
-
-        auto *addr2 = get_chimera().get_signature("leak_file_descriptors_2_sig").data();
-        overwrite(addr2 + 2, fix);
-
-        auto *addr3 = get_chimera().get_signature("leak_file_descriptors_3_sig").data();
-        overwrite(addr3 + 1, fix);
+        
+        if(chimera.feature_present("core_file_size_demo")) {
+            auto *addr4 = chimera.get_signature("max_map_size_demo_1_sig").data();
+            overwrite(addr4 + 1, fix);
+            
+            auto *addr5 = chimera.get_signature("max_map_size_demo_2_sig").data();
+            overwrite(addr5 + 1, fix);
+        }
     }
 }
