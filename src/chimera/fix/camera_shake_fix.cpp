4ba258414fa8c7e0afeeb225d81dd6b8a87f99bc
Cambidd <95023823+Cambidd@users.noreply.github.com>
2024-04-01 16:53:25 +0800

    Camera shake fixes
    - Fix camera shake random translation effect being tied to framerate. Previously only the camera rotation effect was fixed.
    
    - Fix camera shake due to player_effect scripting being tied to framerate (Pillar of Autumn rumble, pelican rumble in cutscenes etc). Previously only camera shake from damage_effect tags were affected.
    
    - Fix camera shake interpolation only "half" working. Previously, the interpolation would only affect the first half of the transition before snapping to the next. Another effect of this was to make camera shaking at 30fps cap look less intense than it should. Now the interpolation functions over the entire effect. This brings the camera shake in line with the implementation in H1A.

--- src/chimera/fix/camera_shake_fix.cpp
+++ src/chimera/fix/camera_shake_fix.cpp
@@ -2,69 +2,64 @@
 
 #include <cmath>
 
-#include "fov_fix.hpp"
 #include "../chimera.hpp"
 #include "../signature/signature.hpp"
 #include "../signature/hook.hpp"
 #include "../event/tick.hpp"
-#include "../output/output.hpp"
 #include "../math_trig/math_trig.hpp"
 
 namespace Chimera {
     extern "C" {
         void camera_shake_fix_asm() noexcept;
-        void camera_shake_tick_asm() noexcept;
-        std::uint16_t *camera_shake_counter_ptr = nullptr;
-        std::uint32_t camera_shaking = 0;
     }
 
-    static int last_shake_counter = 0;
-    static Quaternion shake_before = {};
-    static Quaternion shake_after = {};
+    static bool tick_passed = false;
+
+    static Quaternion rotation_before = {};
+    static Quaternion rotation_after = {};
+    static Point3D translation_before = {};
+    static Point3D translation_after = {};
+
     extern bool interpolation_enabled;
-    static Quaternion shake_done = {};
     extern float interpolation_tick_progress;
 
-    extern "C" void decrease_counter() {
-        if(--last_shake_counter < 0) {
-            last_shake_counter = 0;
-        }
-    }
+    // Do the thing
+    extern "C" void meme_up_camera_shake(RotationMatrix &matrix, Point3D &vector) noexcept {
+        // Check if a tick has passed. If so, swap buffers and get new camera shake data
+        if(tick_passed) {
+            rotation_before = rotation_after;
+            rotation_after = matrix;
 
-    extern "C" void meme_up_camera_shake_thing(RotationMatrix &matrix) noexcept {
-        if(!camera_shaking) {
-            shake_before = shake_after;
-            shake_after = matrix;
+            translation_before = translation_after;
+            translation_after = vector;
 
-            // If it's been a while since we shook the camera, shake from 0 to our new thing
-            if(last_shake_counter == 0) {
-                shake_before = shake_done;
-                last_shake_counter = 2;
-            }
-
-            // Note that we're shaking the camera this tick
-            camera_shaking = 1;
+            tick_passed = false;
         }
 
         // Interpolate
         if(interpolation_enabled) {
-            Quaternion shake_interpolated;
-            interpolate_quat(shake_before, shake_after, shake_interpolated, interpolation_tick_progress);
-            matrix = shake_interpolated;
+            Quaternion rotation_interpolated;
+            Point3D translation_interpolated;
+            interpolate_quat(rotation_before, rotation_after, rotation_interpolated, interpolation_tick_progress);
+            interpolate_point(translation_before, translation_after, translation_interpolated, interpolation_tick_progress);
+            matrix = rotation_interpolated;
+            vector = translation_interpolated;
         }
 
         // If interpolation is disabled, just make it look like 30 FPS camera shaking
         else {
-            matrix = shake_after;
+            matrix = rotation_after;
+            vector = translation_after;
         }
     }
 
+    void on_tick() noexcept {
+        tick_passed = true;
+    }
+
     void set_up_camera_shake_fix() noexcept {
-        auto &camera_shake_counter_sig = get_chimera().get_signature("camera_shake_counter_sig");
-        add_pretick_event(camera_shake_tick_asm);
-        add_pretick_event(decrease_counter);
-        shake_done.w = 1.0F;
+        add_pretick_event(on_tick);
         static Hook hook;
-        write_jmp_call(camera_shake_counter_sig.data() + 7, hook, reinterpret_cast<const void *>(camera_shake_fix_asm), nullptr, false);
+        write_jmp_call(get_chimera().get_signature("camera_shake_sig").data(), hook, reinterpret_cast<const void *>(camera_shake_fix_asm), nullptr, false);
     }
 }
