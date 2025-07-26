a993f2e7aa59ae2d9cf73d727f7dfaa9b1fb8fb2
Cambidd <95023823+Cambidd@users.noreply.github.com>
2025-07-26 12:50:40 +0800

    Fix block zoom blur breaking scanline effect
    Fixes https://github.com/SnowyMouse/chimera/issues/36 and https://github.com/SnowyMouse/chimera/issues/100
    
    The part that breaks the scanline effect is not needed now the render target is at full resolution.

--- src/chimera/command/client/visual/block_zoom_blur.cpp
+++ src/chimera/command/client/visual/block_zoom_blur.cpp
@@ -19,22 +19,18 @@ namespace Chimera {
 
         auto &zoom_blur_1_s = get_chimera().get_signature("zoom_blur_1_sig");
         auto &zoom_blur_2_s = get_chimera().get_signature("zoom_blur_2_sig");
-        auto &zoom_blur_3_s = get_chimera().get_signature("zoom_blur_3_sig");
 
         static const SigByte zoom_blur_1_mod[] = {  -1,   -1, 0x38,   -1,   -1, 0x38};
         static const SigByte zoom_blur_2_mod[] = {  -1,   -1, 0x38};
-        static const SigByte zoom_blur_3_mod[] = {0x01};
 
         if(new_value != enabled) {
             if(new_value) {
                 write_code_s(zoom_blur_1_s.data(), zoom_blur_1_mod);
                 write_code_s(zoom_blur_2_s.data(), zoom_blur_2_mod);
-                write_code_s(zoom_blur_3_s.data(), zoom_blur_3_mod);
             }
             else {
                 zoom_blur_1_s.rollback();
                 zoom_blur_2_s.rollback();
-                zoom_blur_3_s.rollback();
             }
             enabled = new_value;
         }
