692fa62e3f44e068c8a4319382866953d8c3c873
Cambidd <95023823+Cambidd@users.noreply.github.com>
2026-05-10 13:12:00 +0800

    Nuke UnsupportedCard and UMA config.txt options
    Prevents the unsupported card popup on launch and the game treating the card as an early 2000s iGPU. Applicable to pretty much every Intel GPU ever.

--- src/chimera/fix/abolish_safe_mode.cpp
+++ src/chimera/fix/abolish_safe_mode.cpp
@@ -10,5 +10,16 @@ namespace Chimera {
         auto *safe_mode = get_chimera().get_signature("auto_save_mode_sig").data();
         const SigByte fuck_safe_mode[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
         write_code_s(safe_mode, fuck_safe_mode);
+
+        // Nuke UnsupportedCard and UMA config.txt settings
+        struct ConfigEntry {
+            char *config;
+            std::byte *function_ptr;
+        };
+
+        const SigByte nuke_bs[] = { 0xC3 };
+        auto *configs = *reinterpret_cast<ConfigEntry **>(get_chimera().get_signature("config_txt_function_ptr_sig").data() + 3);
+        write_code_s(configs[7].function_ptr, nuke_bs);
+        write_code_s(configs[21].function_ptr, nuke_bs);
     }
 }
